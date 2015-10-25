c21964478296743085f127fb9d5ca2baa0651112
Renqian Luo (MSR Student-Person Consulting) <v-reluo@microsoft.com>
2015-10-25 19:47:05 +0800

    use x64, use Eratosthenes

--- Prime/Prime.cpp
+++ Prime/Prime.cpp
@@ -2,89 +2,71 @@
 //
 
 #include "stdafx.h"
-#include <time.h>
 #include <stdlib.h>
-#include <math.h>
 #include <malloc.h>
-int GetNPrime(int n)
+
+unsigned int GetNthPrime(int n)
 {
-	int count = 1, number_to_be_judged = 3, pointer_of_the_prime_array, *primes;
-	primes = (int*)malloc(sizeof(int)*n);
-	primes[0] = 2;
-	primes[1] = 3;
-	while (count < n){
-		pointer_of_the_prime_array = 1;
-		while (primes[pointer_of_the_prime_array] * primes[pointer_of_the_prime_array] <= number_to_be_judged){
-			if (number_to_be_judged % primes[pointer_of_the_prime_array] == 0){
-				number_to_be_judged += 2;
-				pointer_of_the_prime_array = 1;
-			}
-			else
-				pointer_of_the_prime_array++;
-		}
-		primes[count++] = number_to_be_judged;
-		number_to_be_judged += 2;
-	}
-	return primes[n - 1];
-}
-int pow_mod(int a, int i, int n)
-{
-	if (i == 0)
-		return 1 % n;
-	int temp = pow_mod(a, i >> 1, n);
-	temp = temp*temp%n;
-	if (i & 1)
-		temp = temp*a%n;
-	return temp;
-}
-bool test(int n, int a, int d)
-{
-	if (n == 2)
-		return true;
-	if (n == a)
-		return true;
-	if ((n & 1) == 0)
-		return false;
-	while (!(d & 1))
-		d = d >> 1;
-	int t = pow_mod(a, d, n);
-	while ((d != n - 1) && (t != 1) && (t!= n - 1))
-	{
-		t = t*t%n;
-		d = d << 1;
-	}
-	return (t == n - 1 || (d & 1) == 1);
-}
-bool isPrime(int n)
-{
-	int i;
-	int a[5] = { 2, 3, 5, 7, 61 };
-	for (i = 0; i <= 4;i++)
-		if (!test(n, a[i], n - 1))
-			return false;
-	return true;
-}
-int GetNPrime2(int n)
-{
-	if (n == 1)return 2;
-	int count, i;
-	count = 1;
+	bool *primes;
+	unsigned int initlen;
+	if (n <= 0)
+		return 0;
+	else if (n <= 10)
+		initlen = 30;
+	else if (n <= 100)
+		initlen = 542;
+	else if (n <= 1000)
+		initlen = 7920;
+	else if (n <= 5000)
+		initlen = 48612;
+	else if (n <= 10000)
+		initlen = 104730;
+	else if (n <= 50000)
+		initlen = 611954;
+	else if (n <= 100000)
+		initlen = 1299710;
+	else if (n <= 500000)
+		initlen = 7368788;
+	else if (n <= 1000000)
+		initlen = 15485864;
+	else if (n <= 5000000)
+		initlen = 86028122;
+	else if (n <= 10000000)
+		initlen = 179424674;
+	else if (n <= 50000000)
+		initlen = 982451654;
+	else if (n <= 100000000)
+		initlen = 2038074744;
+	else
+		return 0;
+	primes = (bool*)malloc(sizeof(bool)*(initlen));
+	unsigned int i = 0;
+	for (i = 0; i < initlen; i++)
+		primes[i] = 0;
+	int count = 0;
+	unsigned int j;
 	i = 1;
-	srand(time(NULL));
 	while (count < n){
-		i += 2;
-		if (isPrime(i))
+		i++;
+		if (primes[i] == 0){
 			count++;
+			j = 2 * i;
+			while (j < initlen){
+				primes[j] = 1;
+				j = j + i;
+			}
+		}
 	}
+	free(primes);
 	return i;
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int n, res;
-	while (scanf("%d",&n) && n != 0){
-		res = GetNPrime(n);
-		printf("%d\n", res);
-	}
+	int n;
+	unsigned int res;
+	scanf("%d", &n);
+	res = GetNthPrime(n);
+	printf("%u\n", res);
 	return 0;
 }
 
